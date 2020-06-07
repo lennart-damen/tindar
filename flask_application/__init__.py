230ce27c119463943f922c6e8d3f0362cb18dc3d
Lennart Damen <lennart.damen@hotmail.com>
2020-06-07 12:06:09 +0200

    Let run.py start on 0.0.0.0 port 8080

--- flask_application/__init__.py
+++ flask_application/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 
-print("lalal")
 app = Flask(__name__)
-print(app)
 
 from flask_application import views
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from flask_application import app
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port="8080", debug=False)
